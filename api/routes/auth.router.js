98a06a7e35b7f5aaf23e6b43885b69c23a6cba5f
Stuart Darsey <stuart.darsey@gmail.com>
2021-12-14 18:27:43 -0500

    adds validateBody mw

--- api/routes/auth.router.js
+++ api/routes/auth.router.js
@@ -1,21 +1,35 @@
 const authRouter = require('express').Router();
 const Auth = require('../../data/models/Auth.model');
-const generators = require('../../utils/generators')
+const generators = require('../../utils/generators');
+const mw = require('../middleware/auth.mw')
 
-authRouter.post('/register', (req, res, next) => {
+authRouter.post('/register', mw.validateBody, (req, res, next) => {
     let user = req.body;
     const hash = generators.password(user.password);
     user.password = hash;
 
     Auth.add(user)
-        .then((newUser) => {
+        .then(newUser => {
             const hammeredUserObj = {
                 username: newUser[0].username,
-                user_id: newUser[0].user_id
+                userId: newUser[0].userId
             };
             res.status(201).json(hammeredUserObj)
         })
         .catch(err => next(err))
+});
+
+authRouter.post('/login', (req, res) => {
+    const { userId, username } = req.body;
+    const token = generators.token(req.body);
+    res.status(200).json({
+        message: `Welcome back, ${username}`,
+        token,
+        user: {
+            userId: userId,
+            username: username
+        }
+    });
 });
 
 module.exports = authRouter;
