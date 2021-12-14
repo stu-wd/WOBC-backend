c8d4465f06740fa057a30ab582324f5b4e237ec3
Stuart Darsey <stuart.darsey@gmail.com>
2021-12-14 18:14:16 -0500

    receives correct message back after registering new user

--- api/routes/auth.router.js
+++ api/routes/auth.router.js
@@ -1,6 +1,21 @@
 const authRouter = require('express').Router();
+const Auth = require('../../data/models/Auth.model');
+const generators = require('../../utils/generators')
 
 authRouter.post('/register', (req, res, next) => {
     let user = req.body;
-    
+    const hash = generators.password(user.password);
+    user.password = hash;
+
+    Auth.add(user)
+        .then((newUser) => {
+            const hammeredUserObj = {
+                username: newUser[0].username,
+                user_id: newUser[0].user_id
+            };
+            res.status(201).json(hammeredUserObj)
+        })
+        .catch(err => next(err))
 });
+
+module.exports = authRouter;
