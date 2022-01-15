c3d3f3eb9df3d90fd065bf24fa4c64a0f614d6ba
stu-wd <stuart.darsey@gmail.com>
2022-01-15 01:19:14 -0500

    changes future to status

--- api/middleware/bikes.mw.js
+++ api/middleware/bikes.mw.js
@@ -1,31 +1,34 @@
-const Bikes = require('../../data/models/bikes.model');
+const Bikes = require("../../data/models/bikes.model");
 
 const validateBody = async (req, res, next) => {
-    try {
-        const { serial, future } = req.body;
-        if (!serial || !future) {
-            next({ status: 400, message: 'Serial number and future must be entered' });
-        } else next()
-    } catch (err) {
-        next({ status: 422, message: 'Error in the req.body' });
-    };
+  try {
+    const { serial, status } = req.body;
+    if (!serial || !status) {
+      next({
+        status: 400,
+        message: "Serial number and status must be entered",
+      });
+    } else next();
+  } catch (err) {
+    next({ status: 422, message: "Error in the req.body" });
+  }
 };
 
 const serialFree = async (req, res, next) => {
-    const { serial } = req.body;
-    try {
-        const checkSerial = await Bikes.findBy({ serial });
-        if (!checkSerial.serial) {
-            next()
-        } else {
-            next({ status: 422, message: 'Serial is taken' });
-        }
-    } catch (err) {
-        next({ status: 422, message: 'Error in serial free logic'})
+  const { serial } = req.body;
+  try {
+    const checkSerial = await Bikes.findBy({ serial });
+    if (!checkSerial.serial) {
+      next();
+    } else {
+      next({ status: 422, message: "Serial is taken" });
     }
+  } catch (err) {
+    next({ status: 422, message: "Error in serial free logic" });
+  }
 };
 
 module.exports = {
-    validateBody,
-    serialFree
-}
+  validateBody,
+  serialFree,
+};
--- data/migrations/20211216000541_02-bikes.js
+++ data/migrations/20211216000541_02-bikes.js
@@ -1,27 +1,27 @@
 exports.up = async (knex) => {
-    await knex.schema
-        .createTable('Bikes', table => {
-            table.increments()
-            table.string('serial').notNullable().unique()
-            table.string('future').notNullable()
-            table.string('condition')
-            table.string('type')
-            table.string('size')
-            table.string('brand')
-            table.string('gender')
-            table.string('kidadult')
-            table.string('received')
-            table.string('storage')
-            table.integer('user_id')
-                table.foreign('user_id')
-                    .references('user_id')
-                    .inTable('users')
-                    .onUpdate('NO ACTION')
-                    .onDelete('NO ACTION')
-            table.datetime('created_at', { precision: 6 }).defaultTo(knex.fn.now(6))
-        })
+  await knex.schema.createTable("Bikes", (table) => {
+    table.increments();
+    table.string("serial").notNullable().unique();
+    table.string("status").notNullable();
+    table.string("condition");
+    table.string("type");
+    table.string("size");
+    table.string("brand");
+    table.string("gender");
+    table.string("kidadult");
+    table.string("received");
+    table.string("storage");
+    table.integer("user_id");
+    table
+      .foreign("user_id")
+      .references("user_id")
+      .inTable("users")
+      .onUpdate("NO ACTION")
+      .onDelete("NO ACTION");
+    table.datetime("created_at", { precision: 6 }).defaultTo(knex.fn.now(6));
+  });
 };
 
 exports.down = async (knex) => {
-    await knex.schema.dropTableIfExists('Bikes');
+  await knex.schema.dropTableIfExists("Bikes");
 };
