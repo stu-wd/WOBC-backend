79426371cc195e427dd10d8b8b58d19ade10084e
stu-wd <stuart.darsey@gmail.com>
2022-01-15 20:21:32 -0500

    pws

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,22 +1,22 @@
-const generators = require('../../utils/generators');
+const generators = require("../../utils/generators");
 
-exports.seed = async function(knex) {
-  return knex('users').del()
+exports.seed = async function (knex) {
+  return knex("users")
+    .del()
     .then(function () {
-      return knex('users').insert([
+      return knex("users").insert([
         {
-          name: 'admin',
-          username: 'admin',
-          password: 'admin'
-          // password: generators.password('admin')
+          name: "admin",
+          username: "admin",
+          // password: 'admin'
+          password: generators.password("admin"),
         },
         {
-          name: 'Blake Pierce',
-          username: 'blake',
-          password: 'elrose'
-          // password: generators.password('elrose')
-        }
-        
+          name: "Blake Pierce",
+          username: "blake",
+          // password: 'elrose'
+          password: generators.password("elrose"),
+        },
       ]);
     });
 };
